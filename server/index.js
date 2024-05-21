8db5c15fe50206c0ac78da7c280fd5d719e780e7
Viswanathan P <67451889+drummerviswa@users.noreply.github.com>
2024-05-21 18:30:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import multer from "multer";
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin:"https://social-drummerviswa.vercel.app/"
+  origin:"https://social-drummerviswa.vercel.app"
 }));
 app.use(cookieParser());
 
