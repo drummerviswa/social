2fe9fa7ef6a6eeeb7721d45c439bfcbc506abab1
Viswanathan P <drummerviswa@gmail.com>
2024-05-21 12:14:56 +0530

    Main

--- server/index.js
+++ server/index.js
@@ -15,9 +15,7 @@ app.use((req,res,next)=>{
     next()
 })
 app.use(express.json());
-app.use(cors({
-    origin:"https://social-drummerviswa.vercel.app/"
-}));
+app.use(cors());
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
