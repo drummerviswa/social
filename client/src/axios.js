710fd0182af0191df5e3972dea5675f12928160e
Viswanathan P <drummerviswa@gmail.com>
2024-05-21 18:49:11 +0530

    Vercel

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 export const makeRequest = axios.create({
   baseURL: "https://social-5r67.onrender.com/api/",
   withCredentials: true,
+  headers:{
+    Authorization:"Bearer secretkey"
+  }
 });
 makeRequest.interceptors.response.use(function(response) {
   return response;
