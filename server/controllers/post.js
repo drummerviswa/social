726b4c07df0b76132dbaa0a57b979b2eaf4d258d
Viswanathan P <67451889+drummerviswa@users.noreply.github.com>
2024-05-22 07:51:20 +0530

    Update post.js

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -13,8 +13,8 @@ export const getPosts = (req, res) => {
 
     const q =
       userId !== "undefined"
-        ? `SELECT DISTINCT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
-        : `SELECT DISTINCT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+        ? `SELECT DISTINCT p.*, u.id AS userId, name, coverPic as profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
+        : `SELECT DISTINCT p.*, u.id AS userId, name, coverPic as profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
     LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
     ORDER BY p.createdAt DESC`;
 
@@ -36,7 +36,7 @@ export const getUserPosts = (req, res) => {
     if (err) return res.status(403).json("Token is not valid!");
 
 
-    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`;
+    const q = `SELECT p.*, u.id AS userId, name, coverPic as profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`;
 
     const values =
       userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
